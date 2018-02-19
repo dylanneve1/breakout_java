f709934f499a2edafa2b4fd07536e611557ddabf
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-19 20:45:35 +0000

    Add base

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame obj = new JFrame();
+		Game game = new Game();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Breakout");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(game);
+	}
+
+}
