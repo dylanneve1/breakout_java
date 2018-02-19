d25b59996efbfcdd7f74a0370ae5db9bf0f3e587
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-19 21:20:46 +0000

    Add movement and ball physics

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Timer;
+import javax.swing.Timer;
 
 import javax.swing.JPanel;
 
@@ -12,7 +15,7 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	
 	private int totalBricks = 21;
 	
-	private Timer time;
+	private Timer timer;
 	private int delay = 8;
 	
 	private int playerX = 310;
@@ -20,29 +23,88 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	private int ballposX = 120;
 	private int ballposY = 350;
 	private int ballXdir = -1;
-	private int ballYdie = -2;
+	private int ballYdir = -2;
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public Game() {
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer = new Timer(delay, this);
+		timer.start();
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		g.fillRect(1,  1,  692,  592);
 		
+		//g.setColor(Color.yellow);
+		//g.fillRect(0,  0,  3,  592);
+		//g.fillRect(0,  0,  692,  3);
+		//g.fillRect(691,  0,  3,  592);
+		
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		g.setColor(Color.yellow);
+		g.fillOval(ballposX, ballposY, 20, 20);
+		
+		g.dispose();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		timer.start();
+		if(play) {
+			if(new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
+				ballYdir *= -1;
+			}
+			
+			ballposX += ballXdir;
+			ballposY += ballYdir;
+			if(ballposX < 0) {
+				ballXdir *= -1;
+			}
+			if(ballposY < 0) {
+				ballYdir *= -1;
+			}
+			if(ballposX < 670) {
+				ballXdir *= -1;
+			}
+		}
+		repaint();
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if(playerX >= 600) {
+				playerX = 600;
+			} else {
+				moveRight();
+			}
+		}
+        if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+        	if(playerX < 10) {
+				playerX = 10;
+			} else {
+				moveLeft();
+			}
+		}
 	}
 
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void moveRight() {
+		play = true;
+		playerX += 20;
 	}
+	
+	public void moveLeft() {
+		play = true;
+		playerX -= 20;
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {}
 
 	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}	
+	public void keyTyped(KeyEvent e) {}	
 }
