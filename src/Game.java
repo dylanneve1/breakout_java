fb4d6a8042e9e9e0ffd76ca83a1bf67d824f166e
Dylan Neve <dylanneve1@users.noreply.github.com>
2018-02-19 20:50:43 +0000

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,48 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Timer;
+
 import javax.swing.JPanel;
 
-public class Game extends JPanel {
+public class Game extends JPanel implements KeyListener, ActionListener {
+	private boolean play = false;
+	private int score = 0;
 	
+	private int totalBricks = 21;
+	
+	private Timer time;
+	private int delay = 8;
+	
+	private int playerX = 310;
+	
+	private int ballposX = 120;
+	private int ballposY = 350;
+	private int ballXdir = -1;
+	private int ballYdie = -2;
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}	
 }
